b8356ab8488e7b769fb8a96e8bfecff0dcf5f858
Soumya Ghosh Dastidar <44349253+gdsoumya@users.noreply.github.com>
2021-10-20 17:03:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { check } from "k6";
 import http from "k6/http";
 export default function() {
-  let res = http.get("http://10.107.236.35:9000/");
+  let res = http.get("http://helloservice.demo.svc.cluster.local:9000/");
   check(res, {
     "is status 200": (r) => r.status === 200
   });
